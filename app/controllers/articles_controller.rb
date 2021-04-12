1729234a29752f84ce36043bd5d06582a4f01207
Dmytro Khmelenko <d.khmelenko@gmail.com>
2021-04-12 16:55:50 +0200

    Fix formatting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,54 +1,56 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, only: [:new, :create]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-    def new
-      @article = Article.new
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
     end
+  end
 
-    def create
-      @article = Article.new(article_params)
-      @article.user = User.first
-      if @article.save
-        flash[:success] = "Article was successfully created"
-        redirect_to article_path(@article)
-      else
-        render 'new'
-      end
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      flash[:success] = "Article was not updated"
+      render 'edit'
     end
+  end
 
-    def show
-    end
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
 
-    def edit
-    end
+  def destroy
+    @article.destroy
+    flash[:success] = "Article was deleted"
+    redirect_to articles_path
+  end
 
-    def update
-      if @article.update(article_params)
-       flash[:success] = "Article was updated"
-       redirect_to article_path(@article)
-      else
-       flash[:success] = "Article was not updated"
-       render 'edit'
-      end
-    end
+  private
 
-    def index
-      @articles = Article.paginate(page: params[:page], per_page: 5)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
-    def destroy
-      @article.destroy
-      flash[:success] = "Article was deleted"
-      redirect_to articles_path
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    private
-      def article_params
-        params.require(:article).permit(:title, :description)
-      end
-
-      def set_article
-        @article = Article.find(params[:id])
-      end
-
-    end
+end
